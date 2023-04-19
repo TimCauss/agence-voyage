ec6b3aa9c0bd83eab29d9365b513db19ec985101
TimCauss <t.caussignac@gmail.com>
2023-04-20 00:19:13 +0200

    feat: nav bar and logo color changing on scroll

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 //on stock le header
 const header = document.querySelector("header");
+//on stock les elements svg-icon
 const logo = document.getElementsByClassName("svg-icon");
-
-console.log(logo[0]);
-console.log(logo[1]);
 
 //on écoute le scroll
 window.addEventListener("scroll", () => {
-    //si le scroll est supérieur à 0
-    if(window.scrollY > 0){
-        //on ajoute la classe scroll au header
-        header.classList.add("scroll");
-        logo[0].classList.add("logo-scroll");
-        logo[1].classList.add("logo-scroll");
-    }else{
-        //sinon on l'enlève
-        header.classList.remove("scroll");
-        logo[0].classList.remove("logo-scroll");
-        logo[1].classList.remove("logo-scroll");
+  //si le scroll est supérieur à 0
+  if (window.scrollY > 0) {
+    //on ajoute la classe scroll au header
+    header.classList.add("scroll");
+    //on parcourt le tableau logo
+    for (let i = 0; i < logo.length; i++) {
+      //on ajoute la classe logo-scroll aux éléments du tableau logo
+      logo[i].classList.add("logo-scroll");
     }
+  } else {
+    //sinon on l'enlève
+    header.classList.remove("scroll");
+    //on parcourt le tableau logo
+    for (let item = 0; item < logo.length; item++) {
+      //on enlève la classe logo-scroll aux éléments du tableau logo
+      logo[item].classList.remove("logo-scroll");
+    }
+  }
 });
