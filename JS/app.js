35b9b156bb69ac804171177a8bbbb8682b93732a
Dindon03 <dondelinger-jonathan@hotmail.fr>
2023-04-26 14:39:53 +0200

    26/042023

--- JS/app.js
+++ JS/app.js
@@ -20,3 +20,24 @@ setInterval(function() {
     enleverActiveImages();
     img_slider[etape].classList.add('active');
 }, 5000)
+
+
+let commentaireCourant = 1;
+
+// Fonction pour afficher le commentaire suivant
+function afficherCommentaireSuivant() {
+    let commentaireActuel = document.getElementById(`commentaire${commentaireCourant}`);
+    commentaireActuel.style.display = 'none';
+    commentaireCourant++;
+    if (commentaireCourant > 9) {
+        commentaireCourant = 1;
+    }
+    let commentaireSuivant = document.getElementById(`commentaire${commentaireCourant}`);
+    commentaireSuivant.style.display = 'block';
+}
+
+// Appeler la fonction pour afficher le premier commentaire
+afficherCommentaireSuivant();
+
+// Définir un intervalle pour afficher automatiquement les commentaires toutes les 3 secondes
+setInterval(afficherCommentaireSuivant, 15000);
